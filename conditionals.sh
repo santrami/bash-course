243b8f8eaa696e3dbad0103df7b2304afb6388a2
santrami <santrami@hotmail.com>
2023-04-02 19:52:24 +0200

    conditionals

--- conditionals.sh
+++ conditionals.sh
@@ -5,6 +5,9 @@ age=17
 if (( $age >=  18 ))
 then 
 	echo "eres un adulto"
+elif (( $age == 17  ))
+then
+	echo "casi eres un adulto"
 else
 	echo "no eres un adulto, vuelve en 18-$age"
 fi
---
+++ logicalOperators.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+age=10
+
+if [[ $age -gt 18 && $age -lt 40  ]]
+then
+	echo "edad válida"
+else
+	echo "edad invalida"
+fi
