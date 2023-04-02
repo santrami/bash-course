b223df310cf47a70f4b1742bde0e5bf6506bd3c3
santrami <santrami@hotmail.com>
2023-04-02 20:19:53 +0200

    for with range and interval

--- for.sh
+++ for.sh
@@ -1,11 +1,22 @@
 #! /bin/bash
 
+#for with list
+
 for i in 1 2 3 4 5 
 do
 	echo $i 
 done
 
+#for with range
+
 for i in {1..100}
+do
+	echo $i
+done
+
+#for with range and interval
+
+for i in {1..1000..10}
 do
 	echo $i
 done
