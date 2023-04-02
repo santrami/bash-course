61356f7f5550c8e2cacbc005e8440a27e02ab052
santrami <santrami@hotmail.com>
2023-04-02 20:30:00 +0200

    for with different syntaxes

--- for.sh
+++ for.sh
@@ -2,21 +2,30 @@
 
 #for with list
 
+echo "do with list"
+
 for i in 1 2 3 4 5 
 do
 	echo $i 
 done
 
-#for with range
+echo "for with range"
 
 for i in {1..100}
 do
 	echo $i
 done
 
-#for with range and interval
+echo "for with range and interval"
 
 for i in {1..1000..10}
+do
+	echo $i
+done
+
+echo "for with another and familiar syntax"
+
+for (( i=0; i<100 ; i++  ))
 do
 	echo $i
 done
